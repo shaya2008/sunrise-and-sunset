e22efa8fdb7b44adfe8aba73dd19ba74283175d8
shaya2008 <86904002+shaya2008@users.noreply.github.com>
2021-11-25 19:07:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,7 +48,7 @@ async function getBackgroundImg(){
     var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
     var responseJSON = await response.json ();
     var datetime = responseJSON.datetime ;
-      hour = datetime.slice(11,13);
+     hour = datetime.slice(11,13);
     console.log(hour)
     if(hour>=0 && hour<18 ){
         bg = "sunrise.png";
